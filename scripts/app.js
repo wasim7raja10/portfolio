d96f97d54cc691a5e53e5e239889c613b7c8aef3
Wasim Raja <wasim7raja10@gmail.com>
2022-08-27 22:56:50 +0530

    updated location path name according to prod Url

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,9 @@
 const navLink = document.getElementsByClassName('nav-link')
 
-if(window.location.pathname == '/pages/projects.html') {
+if(window.location.pathname == '/portfolio/pages/projects.html') {
     navLink[1].classList.add('link-active')
 }
-else if(window.location.pathname == '/pages/blogs.html') {
+else if(window.location.pathname == '/portfolio/pages/blogs.html') {
     navLink[2].classList.add('link-active')
 }
 else {

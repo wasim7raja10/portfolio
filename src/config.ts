8cc6c52dac7b7aea0793be763e170eb6a60b4fab
Wasim Raja <wasim7raja10@gmail.com>
2023-01-19 18:46:12 +0530

    base layout and font add

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Wasim Raja';
+export const SITE_DESCRIPTION = 'Wasim Raja is a software engineer and a web developer. He is a full stack developer and has experience in building web applications using React, Node, and MongoDB. He is also a freelance web developer and is available for hire.';

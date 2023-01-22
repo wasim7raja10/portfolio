623cd3501a22ccbbffb41edb3066d18faa82cf6b
Wasim Raja <wasim7raja10@gmail.com>
2023-01-22 12:40:06 +0530

    added prettier tailwind pluign

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import react from "@astrojs/react";

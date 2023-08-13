e61ce0d6c208700f0c56f81b1fe775b4188f40f7
wasim7raja10 <wasim7raja10@gmail.com>
2023-08-13 08:53:57 +0530

    tina

---
+++ src/pages/blog/Tina-CMS.md
@@ -0,0 +1,6 @@
+---
+title: Tina CMS
+pubDate: 2023-08-13T18:30:00.000Z
+---
+
+Hello World
---
+++ tina/config.ts
@@ -0,0 +1,51 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "a874d7b4-eea0-4616-8c82-1cc98efb2672", // Get this from tina.io
+  token: "2456b0e52857255da1829922d1765a51294f1488", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "src/pages/blog",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "pubDate",
+            label: "Date Posted",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});

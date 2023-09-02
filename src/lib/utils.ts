496101f077d6c88a93805a7ec2692c5c4943681f
wasim7raja10 <wasim7raja10@gmail.com>
2023-09-02 15:46:09 +0530

    experience card done

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,6 +5,22 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// export function formatDateToMonthYear(date) {
-//   return date.toLocaleDateString("en-US", { month: "short", year: "2-digit" });
-// }
+export function formatDate(dateString) {
+  // Convert the input string to a Date object
+  const inputDate = new Date(dateString);
+
+  // Get the current date
+  const currentDate = new Date();
+
+  // Check if the input date is in the future
+  if (inputDate > currentDate) {
+    return "Current";
+  } else {
+    // Format the date as "Month Year"
+    const formattedDate = inputDate.toLocaleDateString("in", {
+      year: "numeric",
+      month: "short",
+    });
+    return formattedDate;
+  }
+}

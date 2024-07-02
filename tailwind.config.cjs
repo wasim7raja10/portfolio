6806e998b4bf8d7ddafb7c5c44ed4b7a8087839b
Wasim Raja <wasim7raja10@gmail.com>
2024-07-03 03:21:39 +0530

    chore: Update dev script in package.json for Windows compatibility

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -21,8 +21,6 @@ module.exports = {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        main: "var(--gradient-start)",
-        "gradient-end": "var(--gradient-end)",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {

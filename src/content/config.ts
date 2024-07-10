0db04eb4e4198cf01372c2a3f12837a8f4734386
Wasim Raja <wasim7raja10@gmail.com>
2024-07-10 09:00:54 +0530

    refactor: Add projects and experiences collections to config.ts
    This commit adds the projects and experiences collections to the config.ts file. These collections define the schema for project and experience data, including fields like name, description, image, skills, liveUrl, githubUrl, startDate, and endDate. This change enhances the data structure and organization of the project, allowing for better management and display of projects and experiences.

--- src/content/config.ts
+++ src/content/config.ts
@@ -18,6 +18,47 @@ const blogsCollection = defineCollection({
     }),
 });
 
+const projectsCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      description: z.array(z.string()).optional(),
+      image: z
+        .object({
+          url: image(),
+          alt: z.string(),
+        })
+        .optional(),
+      skills: z.array(z.string()).optional(),
+      liveUrl: z.string().optional(),
+      githubUrl: z.string().optional(),
+      startDate: z.date(),
+      endDate: z.date().optional(),
+    }),
+});
+
+const experiencesCollection = defineCollection({
+  type: "data",
+  schema: ({ image }) =>
+    z.object({
+      company: z.string(),
+      role: z.string(),
+      description: z.array(z.string()).optional(),
+      image: z
+        .object({
+          url: image(),
+          alt: z.string(),
+        })
+        .optional(),
+      startDate: z.date(),
+      endDate: z.date().optional(),
+      skills: z.array(z.string()).optional(),
+    }),
+});
+
 export const collections = {
   blogs: blogsCollection,
+  projects: projectsCollection,
+  experiences: experiencesCollection,
 };
